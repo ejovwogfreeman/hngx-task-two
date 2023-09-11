d3c7efe280261f268f18bca16946e9af71830ba6
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-09-11 23:39:39 +0100

    message

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Header.css";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../images/tv.png";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import "../css/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" />
+        <span>MovieBox</span>
+      </div>
+      <div>
+        <input type="text" />
+        <BsSearch />
+      </div>
+      <div className="auth">
+        <span>Sign In</span>
+        <div className="icon">
+          <AiOutlineMenu />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
